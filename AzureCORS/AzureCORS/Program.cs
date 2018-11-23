2715816ea087fa7e2f9859c67fdda7c287b396db
manishmawat <manish.mawat@gmail.com>
2018-11-22 22:45:36 -0500

    Adding CORS policy code.

---
+++ AzureCORS/AzureCORS/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.WindowsAzure.Storage;
+using Microsoft.WindowsAzure.Storage.Blob;
+using Microsoft.WindowsAzure.Storage.Shared.Protocol;
+using System.Configuration;
+using System.Threading.Tasks;
+
+namespace AzureCORS
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            var storageConnectionString = ConfigurationManager.AppSettings.Get("StorageConnectionString");
+            CloudStorageAccount cloudStorageAccount = CloudStorageAccount.Parse(storageConnectionString);
+            CloudBlobClient cloudBlobClient = cloudStorageAccount.CreateCloudBlobClient();
+            CreateCORSPolicy(cloudBlobClient).GetAwaiter().GetResult();
+        }
+
+        static async Task CreateCORSPolicy(CloudBlobClient cloudBlobClient)
+        {
+            ServiceProperties serviceProperties = new ServiceProperties();
+            serviceProperties.Cors = new CorsProperties();
+
+            CorsRule corsRule = new CorsRule();
+            corsRule.AllowedHeaders = new List<string>() { "*" };
+            corsRule.ExposedHeaders = new List<string>() { "*" };
+            corsRule.AllowedMethods = CorsHttpMethods.Post;
+            corsRule.AllowedOrigins = new List<string>() { "https://localhost:8080/Books" };
+            corsRule.MaxAgeInSeconds = 3600;
+
+            serviceProperties.Cors.CorsRules.Add(corsRule);
+            //try
+            //{
+               await cloudBlobClient.SetServicePropertiesAsync(serviceProperties);
+            //}
+            //catch(Exception ex)
+            //{
+            //    Console.WriteLine($"Error encountered in setting up the policy: {ex}");
+            //}
+        }
+    }
+}
